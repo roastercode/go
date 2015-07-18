2ceccdb6444527eebe91d044ff0c8ae579c4c761
4ur3l13n <aurelien@hackers.camp>
2015-07-18 19:59:43 +0200

    + adapt to stringutil.reverse

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/4ur3l13n/go/stringutil"
+)
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
 }
