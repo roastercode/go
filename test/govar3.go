d754d470940d077ccb3af90dafdd87fb131ee97f
4ur3l13n <aurelien@hackers.camp>
2015-07-21 09:51:33 +0200

    var experimentation 3

---
+++ test/govar3.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i, j int = 1, 2
+	k := 4
+	c, python, java, golang := true, false, true, "no!"
+
+	fmt.Println(i, j, k, c, python, java, golang)
+}
