62936196cfc9ff4cbaebb0298df88db81a9a199b
4ur3l13n <aurelien@hackers.camp>
2015-07-25 08:19:09 +0200

    zero experimentations

---
+++ test/zero.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int
+	var f float64
+	var b bool
+	var s string
+	var U, V, W float64
+	var k = 0
+	var x, y float32 = -1, -2
+	fmt.Printf("%v %v %v %q\n", i, f, b, s, U, V, W, k , x, y)
+}
