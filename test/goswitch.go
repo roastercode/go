5b84a4570e428f9f6511f513404e6bd6bffa8d38
4ur3l13n <aurelien@hackers.camp>
2015-08-02 13:07:57 +0200

    do you run on an Holly Shit Operating System, go will told you 8-P

---
+++ test/goswitch.go
@@ -0,0 +1,39 @@
+/*
+
+Switch
+
+You probably knew what switch was going to look like.
+
+A case body breaks automatically, unless it ends with a fallthrough statement.
+
+*/
+
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	fmt.Print("Go run on ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	case "freebsd":
+		fmt.Println("FreeBSD.")
+	case "openbsd":
+		fmt.Println("OpenBSD.")
+	case "plan9":
+		fmt.Println("Plan 9.")
+	case "windows":
+		fmt.Println("Holly Shit.")
+	default:
+		// Add other type of OS
+		// freebsd, openbsd
+		// plan9, windows
+		fmt.Printf("%s.", os)
+	}
+}
