fb360c6f0208fd84d7b7d13c83b04c2c0b0a7a7e
4ur3l13n <aurelien@hackers.camp>
2015-08-02 21:36:40 +0200

    deferred action on math-cmplx

---
+++ test/godefer.go
@@ -0,0 +1,33 @@
+/*
+
+Defer
+
+A defer statement defers the execution of a function until the surrounding function returns.
+
+The deferred call's arguments are evaluated immediately, but the function call is not 
+executed until the surrounding function returns.
+
+*/
+
+
+package main
+
+import (
+	"fmt"
+	"math/cmplx"
+)
+
+func main() {
+	defer fmt.Printf("\nHello World!\n\n")
+
+var (
+	a        bool
+	MaxInt   uint64     = 1<<64 - 1
+	z        complex128 = cmplx.Sqrt(-5 +12i)
+)
+
+	const f = "%T(%v)\n"
+	fmt.Printf(f, a, a)
+	fmt.Printf(f, MaxInt, MaxInt)
+	fmt.Printf(f, z, z)
+}
