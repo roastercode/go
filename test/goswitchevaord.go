9a80722f12c3fbd6de7a6ddbfe8b5f7d3dd4fe30
4ur3l13n <aurelien@hackers.camp>
2015-08-02 14:36:52 +0200

    Introduction to import time + modification of the code on 6 days value

---
+++ test/goswitchevaord.go
@@ -0,0 +1,53 @@
+/*
+
+Switch evaluation order
+
+Switch cases evaluate cases from top to bottom, stopping when a case succeeds.
+
+(For example,
+
+ switch i {
+ case 0:
+ case f():
+ }
+
+does not call f if i==0.)
+
+Note: Time in the Go playground always appears to start at 2009-11-10 23:00:00 UTC,
+a value whose significance is left as an exercise for the reader.
+
+*/
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// Introduction to import time
+
+func main() {
+	fmt.Println("When's Saturday?")
+	today := time.Now().Weekday()
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Today.")
+	case today + 1:
+		fmt.Println("Tomorrow.")
+	case today + 2:
+		fmt.Println("In two days.")
+	case today + 3:
+		fmt.Println("In three days.")
+	case today + 4:
+		fmt.Println("In Four days.")
+	case today + 5:
+		fmt.Println("In Six days.")
+	case today + 6:
+		fmt.Println("In Six days.")
+	default:
+		fmt.Println("Too far away.")
+	}
+}
+
+// + all 6 days
