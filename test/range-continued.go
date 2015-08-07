975e1da85dedf6bd7aa662897ebc1d0594ee5063
4ur3l13n <aurelien@hackers.camp>
2015-08-07 23:48:16 +0200

    first version of range-continued

---
+++ test/range-continued.go
@@ -0,0 +1,23 @@
+/*
+
+Range Continued - range-continued.go
+
+You can skip or value by assigning to _
+
+If you only want the index, drop the ", value" entirely.
+
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	pow := make([]int, 10)
+	for i := range pow {
+		pow[i] = 1 << uint(i)
+	}
+	for _, value := range pow {
+		fmt.Printf("%d\n", value)
+	}
+}
