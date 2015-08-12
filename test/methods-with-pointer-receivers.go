2099a2d1adca71a19bd4b85ee39dd1e397a61200
4ur3l13n <aurelien@hackers.camp>
2015-08-12 10:18:05 +0200

    pointer receiver method - 2nd version

--- test/methods-with-pointer-receivers.go
+++ test/methods-with-pointer-receivers.go
@@ -1,3 +1,28 @@
+/*
+
+Methods with pointer receivers - methods-with-pointer-receivers.go
+
+Methods can be associated with a named type or pointer to a named type.
+
+We just saw Abs methods. One on the *Vertex pointer type and the other
+on the 'MyFloat' value type.
+
+There are two reasons to use a pointer receiver. First, to avoid copying
+the value on each method call (more efficient if the value type is a large
+struct). Second, so that the method can modify the value that its receiver
+points to.
+
+Try changing the declarations of the 'Abs' and 'Scale' methods to use
+Vertex as the receiver, instead of *Vertex.
+
+The 'Scale' method has not effect when v is a Vertex. Scale mutates v. When
+v is a value (non-pointer) type, the method sees a copy of the Vertex and
+cannot mutate the original value.
+
+Abs works either way. It only reads v. It doesn't matter whether it is
+reading the original value (through a pointer) or a copy of that value.
+
+*/
 package main
 
 import (
@@ -23,3 +48,4 @@ func main() {
 	v.Scale(5)
 	fmt.Println(v, v.Abs())
 }
+
