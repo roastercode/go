2dc8f5f93f835cd3dbe1bd7b731e4fafd41554e5
Aurélien DESBRIÈRES <aurelien@hackers.camp>
2017-06-21 10:54:22 +0200

    some more go stuff

---
+++ constants/constants.go
@@ -0,0 +1,24 @@
+/*
+Golang - Experimentations
+License    : GNU GPL v3 or later
+Author     : Aurélien DESBRIERES
+Mail       : aurelien@hackers.camp
+Project    : Python Experimentations
+Created on : June 2017
+
+Write with Emacs-Nox ──────────────┐
+Go Constants ──────────────────────┘
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	const LENGTH int = 10
+	const WIDTH int = 5
+	var area int
+
+	area = LENGTH * WIDTH
+	fmt.Printf("value of area : %d\n", area)
+}
---
+++ shebang.go
@@ -0,0 +1,12 @@
+/*
+Golang - Experimentations
+License    : GNU GPL v3 or later
+Author     : Aurélien DESBRIERES
+Mail       : aurelien@hackers.camp
+Project    : Python Experimentations
+Created on : 
+
+Write with Emacs-Nox ──────────────┐
+Go Constants ──────────────────────┘
+*/
+
