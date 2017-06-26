c478963a8e771bffd593c4d44cea107ff313875b
Aurélien DESBRIÈRES <aurelien@hackers.camp>
2017-06-26 23:10:16 +0200

    some more go stuff

---
+++ range/range.go
@@ -0,0 +1,38 @@
+/*
+Golang     : Range
+License    : GNU GPL v3 or later
+Author     : Aurélien DESBRIERES
+Mail       : aurelien@hackers.camp
+Project    : Golang Experimentations
+Created on : June 2017
+
+Write with Emacs-Nox ──────────────┐
+Go lang ───────────────────────────┘
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	// Create a slice
+	numbers := []int{0,1,2,3,4,5,6,7,8}
+
+	// Print the numbers
+	for i := range numbers {
+		fmt.Println("Slice item",i,"is",numbers[i])
+	}
+
+	// Create a map
+	countryCapitalMap := map[string] string {"France":"Paris","Italy":"Rome","Japan":"Tokyo"}
+
+	// print map using keys
+	for country := range countryCapitalMap {
+		fmt.Println("Capital of",country,"is",countryCapitalMap[country])
+	}
+
+	// print map using key-value
+	for country,capital := range countryCapitalMap {
+		fmt.Println("Capital of",country,"is",capital)
+	}
+}
